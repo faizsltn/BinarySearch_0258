eb76556c94894c6c34f28b1ac67e6a6386f1321d
(faizsltn) <(faizsulthondaudm@gmail.com)>
2025-05-02 08:45:06 +0700

    membuat perulangan

--- file.cpp
+++ file.cpp
@@ -5,14 +5,34 @@ int element[10];
 int nPanjang;
 int x;
 
-void binarySearch(){
+void binarySearch()
+{
     cout << "Masukkan element yang mau di cari = ";
-    cin >> x ;
-    int low = 0 ;
-    int high = nPanjang-1;
-    int mid = (low + high)/2;
-}
+    cin >> x;
+    int low = 0;
+    int high = nPanjang - 1;
+    do
+    {
 
-int main(){
+        int mid = (low + high) / 2;
+        if (element[mid] == x)
+        {
+            cout << "Ditemukan" << endl;
+        }
+        if (x < element[mid])
+        {
+            high = mid - 1;
+        }
+        if (x < element[mid])
+        {
+            low = mid + 1;
+        }
+        while (low <= high);
+        if (low > high)
+        {}
+        cout << x << "Tidak ditemukan" << endl;
+    }
 
-}
+    int main()
+    {
+    }
