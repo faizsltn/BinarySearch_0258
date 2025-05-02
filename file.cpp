698ffb0cc05c959af4785c1e90a799d84a5d0f64
(faizsltn) <(faizsulthondaudm@gmail.com)>
2025-05-02 08:00:59 +0700

    membuat prosedur binarySearch

--- file.cpp
+++ file.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 using namespace std;
 
+int element[10];
+int nPanjang;
+int x;
 
+void binarySearch(){
+    cout << "Masukkan element yang mau di cari = ";
+    cin >> x ;
+    int low = 0 ;
+    int high = nPanjang-1;
+    int mid = (low + high)/2;
+}
 
 int main(){
 
